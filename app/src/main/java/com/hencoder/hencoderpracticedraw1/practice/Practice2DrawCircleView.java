52f31294457f516b4cc43fa61d035b4d81c9d5d6
xiongxiangwei <1104177066@qq.com>
2019-04-24 15:33:15 +0800

    first push

--- app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice2DrawCircleView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice2DrawCircleView.java
@@ -1,24 +1,31 @@
 package com.hencoder.hencoderpracticedraw1.practice;
 
 import android.content.Context;
+import android.content.res.Resources;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
+import android.util.TypedValue;
 import android.view.View;
 
 public class Practice2DrawCircleView extends View {
 
-    public Practice2DrawCircleView(Context context) {
-        super(context);
-    }
 
+    private final Paint mPaint;
+    private static final float RADIUS=dpToPix(100);
+    private static final float PADDING=dpToPix(10);
     public Practice2DrawCircleView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+        mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mPaint.setColor(Color.BLACK);
+        mPaint.setStyle(Paint.Style.FILL);
+        mPaint.setStrokeWidth(dpToPix(1));
     }
 
-    public Practice2DrawCircleView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
-        super(context, attrs, defStyleAttr);
-    }
+
+
 
     @Override
     protected void onDraw(Canvas canvas) {
@@ -26,5 +33,12 @@ public class Practice2DrawCircleView extends View {
 
 //        练习内容：使用 canvas.drawCircle() 方法画圆
 //        一共四个圆：1.实心圆 2.空心圆 3.蓝色实心圆 4.线宽为 20 的空心圆
+        //平移坐标系位于中心
+        canvas.translate(getWidth()/2,0);
+
+    }
+
+    private static float dpToPix(int dp) {
+        return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, Resources.getSystem().getDisplayMetrics());
     }
 }
